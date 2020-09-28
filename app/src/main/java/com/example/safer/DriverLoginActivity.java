dbf7c4c6cb9349232e0c8b15c215322b9d2dab3c
ZhouXing19 <zhouxing@uchicago.edu>
2020-09-28 12:33:48 -0500

    Before implementing google firebase auth

---
+++ app/src/main/java/com/example/safer/DriverLoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.safer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class DriverLoginActivity extends AppCompatActivity {
+
+    private EditText mEmail, mPassword;
+    private Button mLogin, mRegistration;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_login);
+
+
+        mEmail = (EditText) findViewById(R.id.email);
+        mPassword = (EditText) findViewById(R.id.password);
+
+        mLogin = (Button) findViewById(R.id.login);
+        mRegistration = (Button) findViewById(R.id.registration);
+
+
+    }
+}
