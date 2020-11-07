047ab6eb8db83a19c1c87de3facb39b77ed4fe0a
ZhouXing19 <zhouxing@uchicago.edu>
2020-11-07 10:38:16 -0600

    [zhou] Create Danger class

--- app/src/main/java/com/example/safer/DangerHelperClass.java
+++ app/src/main/java/com/example/safer/DangerHelperClass.java
@@ -1,4 +1,29 @@
 package com.example.safer;
 
 public class DangerHelperClass {
+    String Time, Description, Location;
+
+    // Empty constructor
+    public DangerHelperClass() {
+    }
+
+    // Overwritten constructor
+    public DangerHelperClass(String time, String description, String location) {
+        this.Time = time;
+        this.Description = description;
+        this.Location = location;
+    }
+
+    // Getter for all attributes
+    public String getTime() {
+        return Time;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
 }
