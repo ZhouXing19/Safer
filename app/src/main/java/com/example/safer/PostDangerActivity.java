a77ba1ed79ffeafe9cde2068a89a58128c5d5e4a
ZhouXing19 <zhouxing@uchicago.edu>
2020-11-13 11:11:45 -0600

    [zhou] open camera

--- app/src/main/java/com/example/safer/PostDangerActivity.java
+++ app/src/main/java/com/example/safer/PostDangerActivity.java
@@ -2,11 +2,15 @@ package com.example.safer;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -14,11 +18,13 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -29,11 +35,16 @@ import java.util.Random;
 
 public class PostDangerActivity extends AppCompatActivity {
     public static final String TAG = "PostDangerActivity";
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
 
     private ImageView mBack;
     private TextView mPickFromMap;
     private EditText mTime, mLocation, mDescription;
-    private FloatingActionButton mPost, mProfile;
+    private FloatingActionButton mPost;
+    private ExtendedFloatingActionButton pictureBtn, videoBtn;
+    private File photoFile;
+    public String photoFileName = "photo.jpg";
+
 
     private final int REQUEST_CODE = 20;
 
@@ -53,7 +64,8 @@ public class PostDangerActivity extends AppCompatActivity {
         mLocation = (EditText) findViewById(R.id.location);
         mDescription = (EditText) findViewById(R.id.descript);
         mPost = (FloatingActionButton) findViewById(R.id.postButton);
-        mProfile = (FloatingActionButton) findViewById(R.id.profileBtn);
+        pictureBtn = (ExtendedFloatingActionButton) findViewById(R.id.pictureBtn);
+        videoBtn = (ExtendedFloatingActionButton) findViewById(R.id.videoBtn);
 
 
         mBack.setOnClickListener(new View.OnClickListener() {
@@ -100,28 +112,63 @@ public class PostDangerActivity extends AppCompatActivity {
 
                     dangerList.PushDanger(danger_id);
                     dangerReference.child(danger_id).setValue(dangerClass);
-
-
                     userReference.child(user_id).child("dangers").child(danger_id).setValue(true);
 
-
                 }
-
             }
 
         });
 
-        mProfile.setOnClickListener(new View.OnClickListener() {
+        pictureBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(PostDangerActivity.this, ProfileActivity.class);
-                startActivity(intent);
-                return;
+                launchCamera();
             }
         });
+
 
 
     }
+
+    private void launchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName);
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        Uri fileProvider = FileProvider.getUriForFile(PostDangerActivity.this, "com.example.safer", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+        // So as long as the result is not null, it's safe to use the intent.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            // Start the image capture intent to take photo
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    public File getPhotoFileUri(String fileName) {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
+            Log.d(TAG, "failed to create directory");
+        }
+
+        // Return the file target for the photo based on filename
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+
+        return file;
+    }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent subintent) {
