5599db8bac0b42c85f8f9cab18b9b3bb8d3aa73c
ZhouXing19 <zhouxing@uchicago.edu>
2020-11-13 20:47:08 -0600

    [zhou] Take video and upload to firebase

--- app/src/main/java/com/example/safer/PostDangerActivity.java
+++ app/src/main/java/com/example/safer/PostDangerActivity.java
@@ -5,7 +5,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.location.Address;
@@ -44,8 +46,6 @@ public class PostDangerActivity extends AppCompatActivity {
     public static final String TAG = "PostDangerActivity";
     public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
     private static final int PICK_FROM_MAP_REQUEST_CODE = 20;
-    private static final int POST_PHOTO_REQUEST_CODE = 33;
-    private static final int POST_VIDEO_REQUEST_CODE = 34;
     private static final int CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE = 55;
 
     private String user_id = "";
@@ -153,20 +153,23 @@ public class PostDangerActivity extends AppCompatActivity {
     }
 
     private void launchVideoCamera() {
-        Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
-        videoFile = getPhotoFileUri(videoFileName, "video");
-        Uri fileProvider = FileProvider.getUriForFile(PostDangerActivity.this, "com.example.safer", videoFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
-        if (intent.resolveActivity(getPackageManager()) != null) {
+            Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+            videoFile = getFileUri(videoFileName, "video");
+            Uri fileProvider = FileProvider.getUriForFile(PostDangerActivity.this, "com.example.safer", videoFile);
+
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
             startActivityForResult(intent, CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE);
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivityForResult(intent, CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE);
         }
     }
 
     private void launchCamera() {
-        // create Intent to take a picture and return control to the calling application
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
         // Create a File reference for future access
-        photoFile = getPhotoFileUri(photoFileName, "image");
+        photoFile = getFileUri(photoFileName, "image");
 
         // wrap File object into a content provider
         // required for API >= 24
@@ -174,41 +177,35 @@ public class PostDangerActivity extends AppCompatActivity {
         Uri fileProvider = FileProvider.getUriForFile(PostDangerActivity.this, "com.example.safer", photoFile);
         intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
 
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
         if (intent.resolveActivity(getPackageManager()) != null) {
-            // Start the image capture intent to take photo
             startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
         }
     }
 
 
-
     // Returns the File for a photo stored on disk given the fileName
-    public File getPhotoFileUri(String fileName, String mediaType) {
+    public File getFileUri(String fileName, String mediaType) {
         // Get safe storage directory for photos
         // Use `getExternalFilesDir` on Context to access package-specific directories.
         // This way, we don't need to request external read/write runtime permissions.
         File mediaStorageDir;
-        switch (mediaType) {
+        switch (mediaType){
             case "image":
                 mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+                break;
             case "video":
                 mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_MOVIES), TAG);
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + mediaType);
         }
-
-
         // Create the storage directory if it does not exist
         if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
             Log.d(TAG, "failed to create directory");
         }
-
         // Return the file target for the photo based on filename
         File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
-
+        // /storage/emulated/0/Android/data/com.example.safer/files/Pictures/PostDangerActivity/photo.jpg
         return file;
     }
 
@@ -281,7 +278,7 @@ public class PostDangerActivity extends AppCompatActivity {
 
 
         if (requestCode == CAPTURE_VIDEO_ACTIVITY_REQUEST_CODE){
-            if (requestCode == RESULT_OK){
+            if (resultCode == RESULT_OK){
                 videoUri = subintent.getData();
                 mStorageRef = FirebaseStorage.getInstance().getReference();
 
